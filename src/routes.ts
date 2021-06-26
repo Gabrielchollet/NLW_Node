8096a0b186293cdde55d7f34c7724df44f207866
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-06-26 15:37:17 -0300

    User authentication conclude

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,18 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 /* Rota para o cadastro das tags com o middleware ensureAdmin entre o path da rota e a função */
 router.post("/tags", ensureAdmin, createTagController.handle);
 /* Rota do usuário para inserir um usuário na aplicação */
 router.post("/users", createUserController.handle);
+/* Rota responsável por fazer o login do usuário */
+router.post("/login", authenticateUserController.handle);
 export { router }
