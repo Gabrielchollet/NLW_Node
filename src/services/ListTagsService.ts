66f9aab0a492f5b290f2346891622b1c733df3d3
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-06-27 18:07:23 -0300

    Listagem das tags

--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -1,7 +1,7 @@
 /* Lista as tags que o usuário poderá adicionar na aplicação */
 import { getCustomRepository } from "typeorm";
 import { TagsRepositories } from "../repositories/TagsRepositories";
-
+import { classToPlain } from "class-transformer";
 
 class ListTagsService {
   async execute() {
@@ -9,7 +9,8 @@ class ListTagsService {
 
     const tags = await tagsRepositories.find();
 
-    return tags;
+    /* A função 'classToPlain' vai dentro da entidade tag e vai criar novos objetos apartir dos existentes vindos do TypeORM */
+    return classToPlain(tags);
   }
 }
 
