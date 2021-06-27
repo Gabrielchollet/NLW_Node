9812d718499a85252da4dcb60fd755d956d0ff69
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-06-27 17:31:44 -0300

    Listagem dos compliments

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSendComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiveComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
 
 const router = Router();
 
@@ -13,14 +15,25 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSendComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiveComplimentsController();
 
 /* Rota para o cadastro das tags com o middleware ensureAdmin entre o path da rota e a função */
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
+
 /* Rota do usuário para inserir um usuário na aplicação */
 router.post("/users", createUserController.handle);
+
 /* Rota responsável por fazer o login do usuário */
 router.post("/login", authenticateUserController.handle);
+
 /* Rota para salvar o compliment */
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+/* Rota para buscar a listagem de elogios enviados */
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle);
+
+/* Rota para buscar a listagem de elogios recebidos */
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle);
 
 export { router };
